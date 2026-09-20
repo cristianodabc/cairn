e43f411ace5aa898753e4f610bb426c6b60551ee
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2026-09-20 14:18:47 -0300

    chore(release): v0.1.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cairn.MixProject do
   def project do
     [
       app: :cairn,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       description: description(),
